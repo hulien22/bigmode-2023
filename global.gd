6930ec1f6d838021bd0433238f5bba700143e3a4
hulien22 <hejulien22@gmail.com>
2023-12-07 01:19:11 -0800

    add disabled/depleted abilities

--- global.gd
+++ global.gd
@@ -4,6 +4,8 @@ var abilities_file: Resource = preload("res://abilities.json");
 var data
 var abilities: Array[Ability];
 var monster_abilities: Array[Ability];
+var disabled_ability: Ability
+var depleted_ability: Ability
 
 func _ready():
 	var file = FileAccess.open(abilities_file.resource_path, FileAccess.READ)
@@ -17,6 +19,8 @@ func load_abilities():
 	for a in data.monster_abilities:
 		assert(a.size() >= 3)
 		monster_abilities.append(Ability.new(a[0],a[1],a[2]))
+	disabled_ability = Ability.new("+Disabled+", "This ability has been disabled!", [])
+	depleted_ability = Ability.new("+Depleted+", "You've used up all uses of this ability for this combat!", [])
 
 func get_upgraded_ability(a:Ability) -> Ability:
 	for x in abilities:
